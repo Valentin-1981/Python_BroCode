a9336269ca7f6fb2d8a04db8087f007c448c04a5
Valentin-1981 <chibinyov@rambler.ru>
2025-02-17 22:45:38 +0300

    first commit

---
+++ file_1.py
@@ -0,0 +1,22 @@
+first_name = "Bro"
+
+print(first_name)
+
+print(f"Hello {first_name}")
+
+age = 3
+quantity = 25
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+
+price = 10.99
+
+print(f"The price is ${price}")
+
+is_student = True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
