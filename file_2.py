4bb9d6c4136a5941c88e089554202ebb043b883a
Valentin-1981 <chibinyov@rambler.ru>
2025-02-18 21:01:27 +0300

    new commit

---
+++ file_2.py
@@ -0,0 +1,15 @@
+name = "Bro Code"
+age = 25
+gpa = 3.2
+is_student = True
+
+print(type(gpa))
+print(type(name))
+print(type(age))
+print(type(is_student))
+
+age = str(age)
+
+age += "1"
+
+print(age)
